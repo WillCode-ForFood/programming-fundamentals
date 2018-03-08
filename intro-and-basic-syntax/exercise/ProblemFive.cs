5c3428f8e632f06b735577e5d3e106a490a7d3e8
Rakrone <37133596+Rakrone@users.noreply.github.com>
2018-03-08 20:45:54 +0200

    replaced csproj file with actual cs solution file

---
+++ intro-and-basic-syntax/exercise/ProblemFive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace syntax_basics_exersises
+{
+    class ProblemFive
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
+            Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
+        }
+    }
+}
