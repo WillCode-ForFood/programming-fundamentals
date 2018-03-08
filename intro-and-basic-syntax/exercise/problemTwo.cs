f2a7c2adb51d94f56025bfdd9c8b371d034699db
Rakrone <37133596+Rakrone@users.noreply.github.com>
2018-03-08 21:01:18 +0200

    documented problem two

--- intro-and-basic-syntax/exercise/problemTwo.cs
+++ intro-and-basic-syntax/exercise/problemTwo.cs
@@ -6,9 +6,12 @@ namespace Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //We recieve two inputs (A - width, B - height) that are the sides of a rectangle
             double sideA = double.Parse(Console.ReadLine());
             double sideB = double.Parse(Console.ReadLine());
+            // the variable "area" calculates the area of the rectangle
             double area = sideA * sideB;
+            // finally print the area as a floating point, formatted to the 2nd char after the decimal point
             Console.WriteLine($"{area:F2}");
         }
     }
