88e38ddcdf0adb679af310968e41ec7448b7daf5
Borislav_m <borislav_m@abv.bg>
2018-03-08 21:43:15 +0200

    adding comment

--- intro-and-basic-syntax/exercise/ProblemThree.cs
+++ intro-and-basic-syntax/exercise/ProblemThree.cs
@@ -6,9 +6,10 @@ namespace Lecture1
     {
         static void Main(string[] args)
         {
+            //we are reading 2 doubles
             double km = double.Parse(Console.ReadLine());
             double miles = km * 1.60934;
-
+            //we are printing miles!!!!!!!!!
             Console.WriteLine($"{miles:F2}");
 
         }
