6fcba94c1143c9e0610dc0ff83eb5bc5e10052e2
Kiril <chuchukov.kiril@gmail.com>
2018-03-11 19:29:59 +0200

    Added problem four solution

---
+++ intro-and-basic-syntax/exercise/ProblemFour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double kcalPerHundred = double.Parse(Console.ReadLine()) / 100;
+            double sugarPerHundred = double.Parse(Console.ReadLine()) / 100;
+
+            Console.WriteLine($"{volume}ml {name}: \r\n{kcalPerHundred * volume}kcal, {sugarPerHundred * volume}g sugars");
+        }
+    }
+}
