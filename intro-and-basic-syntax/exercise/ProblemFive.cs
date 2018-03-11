91809ac27593877f24bdbe3bf543a105885549e2
Kiril <chuchukov.kiril@gmail.com>
2018-03-12 01:52:18 +0200

    Documented the Solution Five

--- intro-and-basic-syntax/exercise/ProblemFive.cs
+++ intro-and-basic-syntax/exercise/ProblemFive.cs
@@ -6,13 +6,15 @@ namespace syntax_basics_exersises
     {
         static void Main(string[] args)
         {
-            //
+            //We get from the console a name on the first line
+            //on the next four lines we get integers for current health, max health, current energy, max energy
             string name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Name: {paca}");
+            //we print a visualisation of the information above
+            Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
             Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
         }
